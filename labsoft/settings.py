4c20cc97eeedcaf2bb5cb9e7802cfc23e453c853
nitishjha74 <kumarnitishjha8877@gmail.com>
2024-07-18 21:00:41 +0530

    static_add_file

--- labsoft/settings.py
+++ labsoft/settings.py
@@ -151,14 +151,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-PRODUCTION = str(os.getenv('PRODUCTION'))
 
-if PRODUCTION == 'True':
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
-    STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
-else:
-    pass
 
 
 MEDIA_URL = '/media/'
